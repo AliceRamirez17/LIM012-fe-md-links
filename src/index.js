d2dbd720d9d3543f88cfe79bfca3f86a69747e47
AliceRamirez17 <alicemarcelaramirez@gmail.com>
2020-05-31 22:57:14 -0500

    Agregando función de recursividad

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const path = require('path');
 
 // Enlistar todos los archivos que encuentre en el directorio
 const allFiles = () => {
-  fs.readdirSync(__dirname).forEach(file => {
+  fs.readdirSync(__dirname).forEach((file) => {
     console.log(file);
   });
 };
 
 allFiles();
 
-const relPath = './carpeta';
+const relPath = './carpeta/data.md';
+
+const dirPath = './carpeta';
+const filePath = './carpeta/data.md';
 
 // Encontrar una ruta en específico
 const fileExists = fs.existsSync('./carpeta');
@@ -22,6 +25,30 @@ const absolute = path.isAbsolute(relPath);
 if (absolute === true) {
   console.log('la ruta es absoluta');
 } else {
+  // Convertir ruta relativa a absoluta
   const convertPath = path.resolve(relPath);
-  console.log(convertPath);
+  console.log('Ruta convertida a absoluta', convertPath);
 }
+
+// Función recursiva
+const recursion = (onePath) => {
+  fs.stat(onePath, (err, stats) => {
+    if (err) {
+      console.log(err);
+    }
+    // caso base
+    if (stats.isFile() === true) {
+      console.log('is path');
+    } else {
+      fs.readdirSync(onePath).forEach((file) => {
+        const arr = ['./carpeta/'];
+        arr.push(file);
+        const fileToPath = path.resolve(arr.join(''));
+        console.log(fileToPath);
+        recursion(fileToPath);
+      });
+    }
+  });
+};
+
+recursion(dirPath);
